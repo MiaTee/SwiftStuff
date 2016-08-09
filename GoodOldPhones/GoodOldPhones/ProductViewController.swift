bbd46b7febfc8ddbc4b35590a2fc861f83e916a4
Selma2015 <selma.nb@aol.com>
2016-08-09 17:36:34 -0400

    Added action to ADD TO CART button

--- GoodOldPhones/GoodOldPhones/ProductViewController.swift
+++ GoodOldPhones/GoodOldPhones/ProductViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class ProductViewController: UIViewController {
 
     @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         productNameLabel.text = "1937 Old Desk Phone"
+        productImageView.image = UIImage(named:"phone-fullscreen3")
         // Do any additional setup after loading the view.
     }
 
    
+    @IBAction func addToCartButtonTapped(sender: AnyObject) {
+        
+        print("ADD TO CART button was tapped!")
+    }
 }
