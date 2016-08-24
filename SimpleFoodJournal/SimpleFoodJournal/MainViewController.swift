75defc926a6a189f9258899a5a5b86dede73b84f
Selma2015 <selma.nb@aol.com>
2016-08-24 00:38:24 -0400

    added mainViewController class and view on Storyboard

---
+++ SimpleFoodJournal/SimpleFoodJournal/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  SimpleFoodJournal
+//
+//  Created by Selma NB on 8/22/16.
+//  Copyright © 2016 Selma Boudjemaa. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+}
+
