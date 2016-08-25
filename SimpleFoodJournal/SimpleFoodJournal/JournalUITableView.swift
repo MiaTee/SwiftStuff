fc3e7337ce1e4c83c69c68a31f6f1650d0684813
Selma2015 <selma.nb@aol.com>
2016-08-24 22:22:55 -0400

    adding journal UIView

---
+++ SimpleFoodJournal/SimpleFoodJournal/JournalUITableView.swift
@@ -0,0 +1,15 @@
+//
+//  JournalUITableView.swift
+//  SimpleFoodJournal
+//
+//  Created by Selma NB on 8/24/16.
+//  Copyright © 2016 Selma Boudjemaa. All rights reserved.
+//
+
+import UIKit
+
+class JournalUITableView: UITableView {
+    
+    
+
+}
